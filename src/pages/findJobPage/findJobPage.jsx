7b75d3be1b2a2bd1fb7435c76c04e6edbf269a70
Vaibhav Dasharathe <vinfinitysailor@gmail.com>
2024-08-15 22:32:19 +0530

    Job Card Updated.

--- src/pages/findJobPage/findJobPage.jsx
+++ src/pages/findJobPage/findJobPage.jsx
@@ -1,73 +1,77 @@
-import React, { useEffect } from 'react';
-import { Row, Container, Col } from 'react-bootstrap';
-import { JobCard, Header, Toast, Footer } from '../../components';
-import { useDispatch, useSelector } from 'react-redux';
-import { updateUserRole } from '../../slices/currentUserSlice';
-import { useState } from 'react';
-import { getService } from '../../serviceAPI/serviceAPI';
+import React, { useEffect } from "react";
+import { Row, Container, Col } from "react-bootstrap";
+import { JobCard, Header, Toast, Footer } from "../../components";
+import { useDispatch, useSelector } from "react-redux";
+import { updateUserRole } from "../../slices/currentUserSlice";
+import { useState } from "react";
+import { getService } from "../../serviceAPI/serviceAPI";
+import { NewJobCard } from "../../components/jobCard/jobCard";
 
 const FindJobPage = () => {
-	// const jobsList = useSelector((state) => state.jobsList.list);
-	let [jobsList, setJobsList] = useState([]);
-	const [showToast, setShowToast] = useState(false);
-	const [ToastMessage, setToastMessage] = useState('');
-	const dispatch = useDispatch();
-	dispatch(updateUserRole({ role: 'client' }));
-	let searchParams = new URLSearchParams(window.location.search);
+    // const jobsList = useSelector((state) => state.jobsList.list);
+    let [jobsList, setJobsList] = useState([]);
+    const [showToast, setShowToast] = useState(false);
+    const [ToastMessage, setToastMessage] = useState("");
+    const dispatch = useDispatch();
+    dispatch(updateUserRole({ role: "client" }));
+    let searchParams = new URLSearchParams(window.location.search);
 
-	useEffect(() => {
-		const applied = searchParams.get('applied');
-		const type = searchParams.get('type');
-		const category = searchParams.get('category');
+    useEffect(() => {
+        const applied = searchParams.get("applied");
+        const type = searchParams.get("type");
+        const category = searchParams.get("category");
 
-		if (applied === 'true') {
-			if (type === 'already-applied') {
-				setToastMessage('You have already applied for this job!');
-			} else if (type === 'new') {
-				setToastMessage('You have successfully applied for this job!');
-			}
-			setShowToast(true);
-		}
-		setTimeout(() => {
-			setShowToast(false);
-		}, 3000);
-		const fetchData = async () => {
-			try {
-				let res = await getService(
-					`/api/jobs/?status=open&category=${category}`
-				);
-				setJobsList(res.data);
-			} catch (error) {
-				console.log('error:', error);
-			}
-		};
-		fetchData();
-	}, []);
+        if (applied === "true") {
+            if (type === "already-applied") {
+                setToastMessage("You have already applied for this job!");
+            } else if (type === "new") {
+                setToastMessage("You have successfully applied for this job!");
+            }
+            setShowToast(true);
+        }
+        setTimeout(() => {
+            setShowToast(false);
+        }, 3000);
+        const fetchData = async () => {
+            try {
+                let res = await getService(
+                    `/api/jobs/?status=open&category=${category}`
+                );
+                setJobsList(res.data);
+            } catch (error) {
+                console.log("error:", error);
+            }
+        };
+        fetchData();
+    }, []);
 
-	console.log(jobsList);
+    console.log(jobsList);
 
-	return (
-		<Container fluid>
-			<Header />
-			{showToast && <Toast message={ToastMessage} />}
-			<Row>
-				<Col>
-					<h2 className="text-center py-3">Open Job Positions</h2>
-				</Col>
-			</Row>
+    return (
+        <Container fluid>
+            <Header />
+            {showToast && <Toast message={ToastMessage} />}
+            <Row>
+                <Col>
+                    <h2 className="text-center py-3">Open Job Positions</h2>
+                </Col>
+            </Row>
 
-			<Row className="d-flex justify-content-center">
-				{jobsList.length !== 0 &&
-					jobsList?.map((job) => <JobCard job={job} />)}
-				{jobsList.length === 0 && (
-					<p className="text-center p-5 m-5">
-						No Jobs found for this category
-					</p>
-				)}
-			</Row>
-			<Footer />
-		</Container>
-	);
+            <Row className="d-flex justify-content-center gap-4">
+                {jobsList.length !== 0 &&
+                    jobsList?.map(
+                        (job) => <NewJobCard job={job} />
+                        // <JobCard job={job} />
+                    )}
+                {jobsList.length === 0 && (
+                    <p className="text-center p-5 m-5">
+                        No Jobs found for this category
+                    </p>
+                )}
+            </Row>
+            <Footer />
+        </Container>
+    );
 };
 
 export default FindJobPage;
